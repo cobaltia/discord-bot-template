0f8b585997a7f65ccabf976d4cb3b396935ac003
JuanPablo2655 <personal@isidrocasiano.com>
2022-06-25 20:12:52 -0400

    feat: load listeners

--- src/lib/BotClient.ts
+++ src/lib/BotClient.ts
@@ -1,10 +1,16 @@
 import { CLIENT_OPTIONS } from '#root/config';
 import { Client, Collection } from 'discord.js';
 import { ListenerOptions } from './structures';
+import { ListenerRegistry } from './structures/registries';
 
 export class BotClient extends Client {
 	public events = new Collection<string, ListenerOptions>();
 	constructor() {
 		super(CLIENT_OPTIONS);
+	}
+
+	async login(token?: string) {
+		await Promise.all([ListenerRegistry(this)]);
+		return await super.login(token);
 	}
 }
